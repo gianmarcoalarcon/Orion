c57dcf38760e44674d027f98323354995be605a6
GianM <gianm@pop-os.localdomain>
2023-06-06 11:26:05 +0700

    Fix setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Script to setup environment
 sudo apt update
-sudo apt install -y cmake make git clang++-7 libgmp-dev g++ parallel
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-7 1000
+sudo apt install -y cmake make git clang libgmp-dev g++ parallel
